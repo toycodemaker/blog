a3d7931134ede2c8f9a23aa25455eb486bc0d68a
toycodemaker <primer_plus202010@163.com>
2026-04-01 16:35:29 +0800

    try github actions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-for dirpath, dirnames, filenames in os.walk('你的目录路径'):
+for dirpath, dirnames, filenames in os.walk('./'):
     print(f'目录: {dirpath}')
     for filename in filenames:
         print(f'文件: {os.path.join(dirpath, filename)}')
