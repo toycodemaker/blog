8e95a505c232afead6e3dff92b8414972053bd67
toycodemaker <primer_plus202010@163.com>
2026-04-01 17:19:12 +0800

    try github actions

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 
-for dirpath, dirnames, filenames in os.walk('./'):
-    print(f'目录: {dirpath}')
-    for filename in filenames:
-        print(f'文件: {os.path.join(dirpath, filename)}')
+with open("output/output.txt", "w", encoding="utf-8") as f:
+    for dirpath, dirnames, filenames in os.walk('./'):
+        print(f'目录: {dirpath}')
+        f.write(f'目录: {dirpath}\n')
+        for filename in filenames:
+            print(f'文件: {os.path.join(dirpath, filename)}')
+            f.write(f'文件: {os.path.join(dirpath, filename)}\n')
