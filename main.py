f05476f8fe8209aea70e12d63a4ba55cee5e8faf
toycodemaker <primer_plus202010@163.com>
2026-04-01 16:33:47 +0800

    try github actions

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello github actions")
+import os
+
+for dirpath, dirnames, filenames in os.walk('你的目录路径'):
+    print(f'目录: {dirpath}')
+    for filename in filenames:
+        print(f'文件: {os.path.join(dirpath, filename)}')
